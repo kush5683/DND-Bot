5bb1c5ca790bae8af80f4378e89eb8641fb7fea0
kush5683 <32845063+kush5683@users.noreply.github.com>
2020-05-11 18:29:28 -0400

    Bot response fix 3.1

--- DND.py
+++ DND.py
@@ -6,7 +6,7 @@ import time
 import psutil
 import datetime
 
-version = "Build 2.0"
+version = "Build 3.1"
 
 TOKEN = open('token.txt').readline()
 def buildEmbed():
@@ -74,7 +74,7 @@ def getBotStat():
 @client.event
 async def on_message(message):
     print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
-    text = message.content.upper()
+    text = message.content.upper().split()
     for x in text:
         if x == 'BOT' and (message.author.name != 'DND OVERLORD'):
             await message.channel.send('I am the DND Overlord!')
