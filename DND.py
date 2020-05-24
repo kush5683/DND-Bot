98de5e9459d490a3b2728e6a83ee93d6a2dc054b
kush5683 <32845063+kush5683@users.noreply.github.com>
2020-05-23 23:00:46 -0400

    New roll

--- DND.py
+++ DND.py
@@ -5,26 +5,57 @@ import os
 import time
 import psutil
 import datetime
-import string
-
-
-version = "Build 3.4"
-
 
 TOKEN = open('token.txt').readline()
-def buildEmbed():
-    embed = discord.Embed(
-            color = discord.Colour.orange(),
-            title='Help'
-    )
-    embed.add_field(name='!help', value='This message',inline=False)
-    embed.add_field(name='!ping', value='Returns Pong!',inline=False)
-    embed.add_field(name='!roll', value='Takes in [d4, d6, d8, d10, d12, d20, d100] followed by a number <10 [1 if not specified] and returns the value rolled',inline=False)
-    embed.add_field(name='!roles', value='Returns Your roles',inline=False)
-    embed.add_field(name='!poop', value='Returns poopy',inline=False)
-    embed.add_field(name='!unshitmypants', value='Does the thing', inline=False)
-    embed.add_field(name='!flip', value='Flips a coin (Heads or Tails)', inline=False)
-    return embed
+def localRoll(numDie, die):
+    values = []
+    if numDie > 10:
+        return "too many dice"
+    sum = 0
+    rolls = 0
+    #d4 d6 d8 d10 d12 d20 d100
+    while rolls < numDie:
+        theDie = die.upper()
+        if(theDie=='D4' or die=='d4'):
+            roll = random.randint(1,4)
+            sum+= roll
+            values.append(roll)
+            rolls+=1
+        elif(theDie=='D6' or die=='d6'):
+            roll = random.randint(1,6)
+            sum+= roll
+            values.append(roll)
+            rolls+=1
+        elif(theDie=='D8' or die=='d8'):
+            roll = random.randint(1,8)
+            sum+= roll
+            values.append(roll)
+            rolls+=1
+        elif(theDie=='D10' or die=='d10'):
+            roll = random.randint(1,10)
+            sum+= roll
+            values.append(roll)
+            rolls+=1
+        elif(theDie=='D12' or die=='d12'):
+            roll = random.randint(1,12)
+            sum+= roll
+            values.append(roll)
+            rolls+=1
+        elif(theDie=='D20' or die=='d20'):
+            roll = random.randint(1,20)
+            sum+= roll
+            values.append(roll)
+            rolls+=1
+        elif(theDie=='D100' or die=='d100'):
+            roll = random.randint(1,100)
+            sum+= roll
+            values.append(roll)
+            rolls+=1
+        else:
+            return "Something went wrong"
+    if(sum==20 and theDie=='D20'):
+        return 'Nat 20'
+    return (sum,values)
 
 def checkConditionTrue(cond, comp):
     if(cond == comp):
@@ -38,73 +69,20 @@ def checkRole(ctx,desiredRole):
             ans = True
     return ans
 
-def localRoll(ctx, numDie, die):
-    dice = [4,6,8,10,12,20,100]
-    checkAdmin = checkRole(ctx, "Admin")
-    if numDie > 10 and checkAdmin == False:
-        return "too many dice"
-    sum = 0
-    rolls = 0
-    theDie = int(die[1:])
-    #d4 d6 d8 d10 d12 d20 d100
-    if theDie in dice:
-        while rolls < numDie:
-            sum+=random.randint(1,theDie)
-            rolls+=1
-    else:
-        return "Something went wrong"
-    if(sum==20 and theDie == 20):
-        return 'Nat 20'
-    if(sum==1):
-        return 'Crit Fail'
-    return sum
 
 client = commands.Bot(command_prefix='!')
-client.remove_command('help')
+#client.remove_command('help')
 processID = psutil.Process(os.getpid())
 
 up = 0
 inProgress = False
 
-
-def getBotStat():
-    sendTo = ''
-    text_channel_list = []
-    for guild in client.guilds:
-        for channel in guild.text_channels:
-            text_channel_list.append(channel)
-    for channel in text_channel_list:
-        if(channel.name == 'bot-status'):
-            sendTo = channel
-    return sendTo
-
-@client.event
-async def on_message(message):
-    print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
-    text = []
-    for str in message.content.upper().split():
-        text.append(str.translate(str.maketrans('', '', string.punctuation)))
-    for x in text:
-        if x == 'BOT' and (message.author.name != 'DND OVERLORD'):
-            await message.channel.send('I am the DND Overlord!')
-    await client.process_commands(message)
-
 @client.event
 async def on_ready():
     global up
     up = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(processID.create_time()))
-    os.system('clear')
-    print(version)
+    os.system('cls')
     print('Bot is ready')
-    await getBotStat().purge(limit=1000)
-    await getBotStat().send(f'I have arrived with {version} loaded')
-    await getBotStat().send(embed=buildEmbed())
-    await getBotStat().send(f'Boot time:{up}')
-
-@client.event 
-async def on_command_error(ctx, error):
-    await ctx.send("Sorry that is an unknown command")
-    await ctx.send(embed=buildEmbed())
     
 @client.event
 async def on_member_join(member):
@@ -119,17 +97,6 @@ async def up(ctx):
     global up
     await ctx.send(f'Up since {up}')
 
-@client.command()
-async def repeat(ctx, message, amount=1):
-    if(checkRole(ctx, 'Admin')):
-        count = 0
-        send = ''
-        while count < amount:
-            send += (message + ' ')
-            count+=1
-        await ctx.send(send)
-    else:
-        await ctx.send('This fun command is only for Admins sorry :(')
 
 @client.command()
 async def ping(ctx):
@@ -138,24 +105,14 @@ async def ping(ctx):
     
 @client.command()
 async def roll(ctx,die, numDie=1):
-    message = localRoll(ctx, int(numDie),die)
+    message = localRoll(int(numDie),die)
     if checkConditionTrue(ctx.channel.name, 'general'):
          await ctx.send(f'This action is not allowed in {ctx.channel}')
     elif message == "too many dice" or message =="Something went wrong":
         await ctx.send(message)
     else:
-        await ctx.send(f' {ctx.author.nick} rolled {localRoll(ctx, int(numDie), die)} from {numDie} {die}')
-
-@client.command()
-async def flip(ctx):
-    coin = ['Heads','Tails']
-    result = random.choice(coin)
-    if checkConditionTrue(ctx.channel.name, 'general'):
-         await ctx.send(f'This action is not allowed in {ctx.channel}')
-    else:
-        await ctx.send(result)
-    
-    
+        tup = localRoll(int(numDie), die)
+        await ctx.send(f' {ctx.author.nick} rolled {tup[0]} from {numDie} {die} \n{tup[1]}')
 
 @client.command()
 async def clear(ctx, amount=100):
@@ -181,10 +138,6 @@ async def poop(ctx):
        await ctx.send(f'{ctx.author.nick} is poopy')
     else:
        await ctx.send('Keith is a poopy head')
-
-@client.command()
-async def unshitmypants(ctx):
-    await ctx.send(file=discord.File('assets/poopPants.jpg'))
        
 @client.command()
 async def kill(ctx):
@@ -192,8 +145,7 @@ async def kill(ctx):
     if not inProgress:
         manager = checkRole(ctx,"Bot Manager")
         if manager:
-            await getBotStat().send('Goodbye cruel world!')
-            await client.change_presence(status=discord.Status.idle)
+            await ctx.send('Goodbye cruel world!')
             exit()
         else:
             await ctx.send('You must be bot manager to perform this task')
@@ -201,10 +153,29 @@ async def kill(ctx):
         await ctx.send('There seems to be a session in progress please wait until it is over to kill me')
 
        
+##@client.command()
+##async def help(ctx):
+##    await ctx.send('To call me use \'!\'')
+##    await ctx.send('**help** : to see this message')
+##    await ctx.send('**ping** : A fun little game')
+##    await ctx.send('**roll** `d4, d6, d8, d10, d12, d20, d100` `numer of die to roll` ***1 if not specified*** : rolls the specified die the specified amount of times')
+##    await ctx.send('**roles** : your highest role')
 
 @client.command()
-async def help(ctx):
-        await ctx.channel.send(embed=buildEmbed())
+async def helpme(ctx):
+    author = ctx.author
+    embed = discord.Embed(
+            color = discord.Colour.orange(),
+            title='Help'
+    )
+    #embed.set_author(name='help')
+    embed.add_field(name='!help', value='This message',inline=False)
+    embed.add_field(name='!ping', value='Returns Pong!',inline=False)
+    embed.add_field(name='!roll', value='Takes in [d4, d6, d8, d10, d12, d20, d100] followed by a number <10 [1 if not specified] and returns the value rolled',inline=False)
+    embed.add_field(name='!roles', value='Returns Your roles',inline=False)
+    embed.add_field(name='!poop', value='Returns poopy',inline=False)
+    
+    await ctx.channel.send(embed=embed)
                     
 @client.command()
 async def start(ctx):
@@ -214,7 +185,6 @@ async def start(ctx):
     if admin:
         if channel:
             await ctx.send('Session Started')
-            await client.change_presence(status=discord.Status.online, activity=discord.Game("In session"))
             inProgress = True
         else:
             await ctx.send('Sessions can only be started from within a campaign text channel')
@@ -229,7 +199,6 @@ async def end(ctx):
     if admin:
         if channel:
             await ctx.send('Session Ended')
-            await client.change_presence(status=discord.Status.idle)
             inProgress = False
         else:
             await ctx.send('Sessions can only be ended from within a campaign text channel')
