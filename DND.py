a8ea71e113c28081e5f72af2ab5d862d950b6671
kush5683 <32845063+kush5683@users.noreply.github.com>
2020-08-12 12:39:12 -0400

    Eldritch blast

--- DND.py
+++ DND.py
@@ -173,6 +173,13 @@ async def roll(ctx,die, numDie=1):
     else:
         await ctx.send(f' {ctx.author.nick} rolled {tup[0]} from {numDie} {die} \n{tup[1]}')
 
+@client.command()
+async def eb(ctx):
+    first = localRoll(ctx, 3, d20) + 13
+    second = localRoll(ctx, 3, d10) + 12
+    await ctx.send(f'Eldritch blast with Trof\'s stats is {first} + {second} = {first + second}')
+
+
 #flips a coin
 @client.command()
 async def flip(ctx):
